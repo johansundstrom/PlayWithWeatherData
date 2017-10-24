edf1c761041d67dad1179ec32fe94e61779e7a9c
johansundstrom <johan.sundstrom@mdh.se>
2017-10-24 21:39:53 +0200

    upd

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PlayWithWeatherData
 Testar att continuerligt hämta väder data och kasta in det i Mongo
+
+## Krav
+* Ska via GET hämta ett JSON-objekt
+* Manipulera objektet, lägg till tid
+
+## Just nu
+* Vilken är den minsta möjliga metoden?
--- index.js
+++ index.js
@@ -12,28 +12,36 @@ const wpath = '/data/2.5/weather';
 const wId = '2715953';
 const wUnits = 'metric';
 const wAppId = '04ae3ab01b0f8351d05831a7f637a508';
-const wUrl = wHost+wpath+'?id='+wId+'&UNITS='+wUnits+'&APPID='+wAppId;
+const wUrl = wHost + wpath + '?id=' + wId + '&UNITS=' + wUnits + '&APPID=' + wAppId;
 
 //console.log(wUrl);
 
-MongoClient.connect(dbUrl, function(err, db) {
-    if (err) throw err;
-    var myobj = { id: 1, temp: 25.1 }; 
-    // db.collection("templog").insertOne(myobj, function(err, res) {
-    //     if (err) throw err;   
-    //     console.log("1 document almost inserted");   
-    //     db.close();
-    // });
+// Fungerar!
+request({ url: wUrl, json: true }, function(error, response, weatherData) {
+    if (!error && response.statusCode === 200) {
+        //console.log(weatherData) // Print the json response
+
+        MongoClient.connect(dbUrl, function(err, db) {
+            if (err) throw err;
+            var myobj = { id: 1, temp: 25.1 }; 
+            //db.collection("templog").insertOne(myobj, function(err, res) {
+            db.collection("templog").insertOne(weatherData, function(err, res) {
+                if (err) throw err;   
+                console.log("1 document almost inserted");   
+                db.close();
+            });
+        })
+
+    }
 })
 
 
 
-// Fungerar!
-// request({url: wUrl, json: true }, function (error, response, weatherData) {
-//     if (!error && response.statusCode === 200) {
-//         console.log(weatherData) // Print the json response
-//     }
-// })
+
+
+
+
+
 
 
 
@@ -41,22 +49,22 @@ MongoClient.connect(dbUrl, function(err, db) {
 
 // Fungerar 2
 
-// // Set the headers
+// Set the headers
 // var headers = {
-//     'User-Agent':       'Super Agent/0.0.1',
-//     'Content-Type':     'application/x-www-form-urlencoded'
-// }
-// // Configure the request
+//         'User-Agent': 'Super Agent/0.0.1',
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     }
+//     // Configure the request
 // var options = {
-//     url: wHost+wpath,
+//     url: wHost + wpath,
 //     method: 'GET',
 //     headers: headers,
 //     json: 'TRUE',
-//     qs: {'id': wId, 'APPID': wAppId, 'UNITS': wUnits}
+//     qs: { 'id': wId, 'APPID': wAppId, 'UNITS': wUnits }
 // }
 
 // // Start the request
-// request(options, function (error, response, body) {
+// request(options, function(error, response, body) {
 //     if (!error && response.statusCode == 200) {
 //         // Print out the response body
 //         //body = JSON.parse(body);
@@ -65,20 +73,19 @@ MongoClient.connect(dbUrl, function(err, db) {
 // })
 
 
-var xmlhttp = new XMLHttpRequest();
+// var xmlhttp = new XMLHttpRequest();
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        console.log = myObj;
-    }
-};
-xmlhttp.open("GET", wUrl, true);
-xmlhttp.send();
+// xmlhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         var myObj = JSON.parse(this.responseText);
+//         console.log = myObj;
+//     }
+// };
+// xmlhttp.open("GET", wUrl, true);
+// xmlhttp.send();
 
 var d = new Date();
 var t = d.getTime();
 
 
-//console.log(d);
-
+console.log(wUrl);
